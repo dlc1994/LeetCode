ed21e7592e5a17d35601879ca708a8bbf28091f6
dailingcheng <dailingcheng@foxmail.com>
2018-07-30 16:02:20 +0800

    2 methods

--- JewerAndStones.py
+++ JewerAndStones.py
@@ -5,3 +5,29 @@
 @file: JewerAndStones.py
 @time: 2018/7/30 11:43
 '''
+J = "aA"
+S = "aAAbbbb"
+#return 3
+
+class Solution:
+    def JewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        # dict={}
+        # for j in range(len(J)):
+        #     if J[j] not in dict:
+        #         dict[J[j]]=0
+        # for s in range(len(S)):
+        #     if S[s] in dict:
+        #         dict[S[s]]+=1
+        # print(sum(dict.values()))
+        # cnt=0
+        # for j in J:
+        #     cnt+=S.count(j)
+        return sum([S.count(j) for j in J])
+            # print(S.count(j))
+s = Solution
+print(s.JewelsInStones(s, J, S))
