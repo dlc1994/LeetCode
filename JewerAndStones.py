3668ff59eb5013455e60bc23822b5ef403694686
dlckeepmoving <dailingcheng@foxmail.com>
2018-09-20 09:52:44 +0800

    Update JewerAndStones.py

--- JewerAndStones.py
+++ JewerAndStones.py
@@ -31,3 +31,69 @@ class Solution:
             # print(S.count(j))
 s = Solution
 print(s.JewelsInStones(s, J, S))
+
+
+> Description of Problem:
+> You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+>
+> The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+> Example 1:
+>```
+ Input: J = "aA", S = "aAAbbbb"
+ Output: 3
+>```
+> Example 2:
+> ```
+Input: J = "z", S = "ZZ"
+Output: 0
+> ```
+> Note:
+>
+> - S and J will consist of letters and have length at most 50.
+> - The characters in J are distinct.
+
+**Approach 1: Python Dict**
+```python
+class Solution:
+    def JewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        dict={}
+        for j in range(len(J)):
+            if J[j] not in dict:
+                dict[J[j]]=0
+        for s in range(len(S)):
+            if S[s] in dict:
+                dict[S[s]]+=1
+        return sum(dict.values())
+```
+Running time is a bit long, therefore I improve it.
+**Approach 2: **
+```python
+class Solution:
+    def JewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        cnt = 0
+        for j in J:
+            cnt+=S.count(j)
+        return cnt
+```
+More simplicity, one line is OK.
+```python
+class Solution:
+    def JewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+		return sum([S.count(j) for j in J])
+```
+Running time: 40ms
