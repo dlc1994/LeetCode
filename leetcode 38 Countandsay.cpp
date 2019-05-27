d71494d506b8de4c3de620be1776e3b4b6ed69e7
dlckeepmoving <dailingcheng@foxmail.com>
2019-05-27 17:11:24 +0800

    Update leetcode 38 Countandsay.cpp

--- leetcode 38 Countandsay.cpp
+++ leetcode 38 Countandsay.cpp
@@ -5,16 +5,20 @@ public:
         int cnt = 1;
         string res = "1";
         while(cnt<n){
-            int count = 1;
+            string result = "";
             for(int i=0;i<res.size();i++){
+                int count = 1;
                 while((i+1)<res.size() && res[i]==res[i+1]){
                     count++;
                     i++;
                 }
-                res += to_string(count)+res[i];
+                result += to_string(count)+res[i];
             }
             cnt++;
+            res = result;
+            cout<<cnt<<" "<<res<<endl;
         }
         return res;
     }
 };
+
